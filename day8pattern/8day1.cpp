6c52b87c9243d4142e14ada2c8fa93ef9549f78c
Rohit'shub <104639942+tngomega@users.noreply.github.com>
2023-10-05 21:33:58 +0530

    Add files via upload

---
+++ day8pattern/8day1.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main() {
+//	
+//	int i=1;
+//	
+//	while(i<=5)
+//	{
+//		for(int r=1;r<=5;r++)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<"\n"; 
+//		i++;
+//}
+
+/*                         */
+
+
+//
+//for(int r=1;r<=4;r++)
+//{
+//	for(int col=1;col<=4;col++)
+//	{
+//		cout<<"10 ";
+//	}
+//	cout<<endl;
+//}
+
+
+
+                                       /*    */
+                                       
+//                                       
+//        for(int r=1;r<=5;r++)
+//		{
+//		    for(int c=1;c<=5;c++)
+//		    {
+//		    	cout<<r<<" ";
+//			}
+//			cout<<endl;
+//		
+//		
+//		}                   
+
+								/*         */
+
+
+		
+								
+//		  for(int r=1;r<=5;r++)
+// 		{
+// 		    for(int c=1;c<=5;c++)
+// 		    {
+// 		    	cout<<c<<" ";
+// 			}
+// 			cout<<endl;				
+//			 	}      						
+//								            
+
+
+
+									/*           */
+									
+//									
+//      	  for(int r=1;r<=5;r++)
+// 		{
+// 		    for(int c=5;c>=1;c--)
+// 		    {
+// 		    	cout<<c<<" ";
+// 			}
+// 			cout<<endl;				
+//			 	}  			
+
+
+
+										/*  H.W          */
+				 						
+//										
+//			  for(int r=1;r<=5;r++)
+// 	       	{
+// 		    for(int c=1;c<=5;c++)
+// 		       {
+// 		    	//cout<<c*c<<" ";
+// 		    	int square=pow(c,2);
+// 		    	cout<<square<<" ";
+// 		    
+// 			   }
+// 			cout<<endl;				
+//			}  								
+//				
+//												
+//			  for(int r=1;r<=5;r++)
+// 	       	{
+// 		    for(int c=1;c<=5;c++)
+// 		       {
+// 		    
+//				int cube=pow(c,3);
+// 		    	cout<<cube<<" ";	
+// 		    
+// 			   }
+// 			cout<<endl;				
+//			}  								
+			
+			
+			/*                     */
+	
+	
+//	for(char var='a';var<='e';var++)
+//	{ 
+//		for(int r=1;r<=5;r++)
+//		{
+//             cout<<var<<" ";
+//			 			
+//		}
+//		cout<<endl;
+//	}			
+
+
+
+                        
+//                        for(int r=1;r<=5;r++)
+//                        {
+//                        	char name='a'+r-1;
+//                        	for(int c=1;c<=5;c++)
+//                        	{
+//                        		cout<<name<<" ";
+//							}
+//							cout<<endl;
+//						}
+
+
+
+										/*   H.W        */
+
+           
+//           for(int r=1;r<=5;r++)
+//           {
+//           	for(int c=97;c<=101;c++)
+//           	{
+//           	   cout<<char(c)<<" ";	
+//			}
+//			cout<<endl;
+//		   }
+
+
+
+
+													/**/
+	
+	       
+//	       int count=1;
+//		for(int r=1;r<=5;r++)
+//		{
+//		   
+//		   for(int c=1;c<=5;c++)
+//		   {
+//		       
+//		       cout<<count++<<" ";
+//			      	
+//		   }	
+//		   cout<<endl;
+//		}											
+
+				
+				
+									
+										
+																
+
+
+
+
+	}
+
