86b4ae9f817dea958be99bc52d6844258a2d2c93
Rohit'shub <104639942+tngomega@users.noreply.github.com>
2023-10-05 21:34:19 +0530

    Add files via upload

---
+++ day9pattern2/9day1.cpp
@@ -0,0 +1,186 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	
+//*
+//**
+//***
+//****
+//*****
+//	for(int r=1;r<=5;r++)
+//	{
+//		for(int c=1;c<=r;c++)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//  }    
+
+
+ 							/**/
+ 							
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5
+//	 for(int r=1;r<=5;r++)
+//	 {
+//         for(int c=1;c<=r;c++)
+//		 {
+//		    cout<<c<<" "; 	
+//    	 }	 	
+//    	 cout<<endl;
+//	 }						
+// 
+// 							
+												/*for dynamic */
+//		int n;
+//		cout<<"Give no. till which pattern you want";
+//		cin>>n;
+//		 for(int r=1;r<=n;r++)
+//	 {
+//         for(int c=1;c<=r;c++)
+//		 {
+//		    cout<<c<<" "; 	
+//    	 }	 	
+//    	 cout<<endl;
+//	 }						
+// 					
+
+					 							/**/		
+//1
+//2 2
+//3 3 3
+//4 4 4 4
+//5 5 5 5 5			 							
+//		
+//		 for(int r=1;r<=5;r++)
+//	 {
+//         for(int c=1;c<=r;c++)
+//		 {
+//		    cout<<r<<" "; 	
+//    	 }	 	
+//    	 cout<<endl;
+//	 }						
+// 														
+ 
+ 												/**/
+//1
+//2 1
+//3 2 1
+//4 3 2 1
+//5 4 3 2 1
+//	for(int r=1;r<=5;r++)
+//	{
+//       for(int c=r; c>=1;c--)
+//	   {
+//           cout<<c<<" ";	   	
+//	   }		
+//	   cout<<endl;
+//    }											   
+//
+												/**/
+												
+//a
+//b b
+//c c c
+//d d d d
+//e e e e e									
+												
+//	for(char num='a';num<='e';num++)
+//	{
+//	   for(char var=num;var>='a';var--)
+//	   {
+//	      	cout<<num<<" ";
+//	   }	
+//	   cout<<endl;
+//	}											
+//
+                                              //or
+                                              
+//         for(int r=1;r<=5;r++)
+//		 {
+//		 	
+//		       char name='a'+r-1;
+//			  
+//			for(int c=1;c<=r;c++)
+//			{
+//		        cout<<name<<" ";		
+//		    }   	
+//		    cout<<endl;
+//      	 }                   
+
+												/**/
+//*****
+//****
+//***
+//**
+//*
+//												
+//	for(int r=1;r<=5;r++)
+//	{
+//		for(int c=5;c>=r;c--)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//	}
+                                               //or
+                                               
+// 	for(int r=1;r<=5;r++)
+//	{
+//		for(int c=1;c<=5-(r-1);c++)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//	} 
+                                               
+            										/**/
+		
+		
+//1 2 3 4 5
+//1 2 3 4
+//1 2 3
+//1 2
+//1											
+//	for(int r=5;r>=1;r--)
+//	{
+//        for(int c=1;c<=r;c++)
+//		{
+//	       cout<<c<<" ";		
+//		}		
+//		cout<<endl;
+//    }					
+																/**/
+
+
+
+//5
+//5 4
+//5 4 3
+//5 4 3 2
+//5 4 3 2 1
+
+//  for(int r=5;r>=1;r--)
+//	{
+//        for(int c=5;c>=r;c--)
+//		{
+//	       cout<<c<<" ";		
+//		}		
+//		cout<<endl;
+//    }												                                   
+
+
+							                                   
+
+
+
+
+
+
+
+
+	
+}
---
+++ day9pattern2/9dayhw.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+//	int r,c,n;
+//	
+//	cout<<"Enter the no. till which pattern follow";
+//	cin>>n;
+//		for(r=1;r<=n;r++)
+//		{
+//			for(c=1;c<=r;c++)
+//			
+//			{
+//				cout<<c<<" ";
+//			}
+//			
+//				cout<<endl;
+//		}
+												/**/
+												
+												
+												
+//		int r,c,n;
+//		
+//		for(char var ='A';var<='E';var++)
+//		{
+//		 
+//	        for(char name='A';name<=var;name++)
+//			{
+//		        cout<<name<<" ";		
+//			}	
+//			cout<<endl;	
+//		}										
+//												
+			
+			
+//					int r,c,n;
+//		
+//		for(r=1;r<=5;r++)
+//		{
+//		 
+//		 char name='A';
+//	        for(c=1;c<=r;c++)
+//			{
+//		        cout<<name<<" ";	
+//				name++;	
+//			}	
+//			cout<<endl;	
+//		}	
+
+
+//       int r,c,n;
+//
+//       cout<<"Enter the no. till which pattern start";
+//       cin>>r;
+//              cout<<"Enter the no. till which pattern end";
+//              cin>>n;
+// 
+//	
+//	for(r;r<=n;r++)
+//		{
+//		   for(c=10;c<=r;c++)
+//		   {
+//		      cout<<r<<" ";   	
+//		   }	
+//		   cout<<endl;
+//		}
+
+
+
+
+//    for(int r=1;r<=4;r++)
+//    {
+//    	char name='A';
+//    	for(int c=4;c>=r;c--)
+//    	{
+//    		
+//    		cout<<name<<" ";
+//    		name++;
+//		}
+//		cout<<endl;
+//	}
+//												
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
