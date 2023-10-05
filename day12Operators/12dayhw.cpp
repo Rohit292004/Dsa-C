a2fc38009bd054751e90d5bc5eeef2e638dfd009
Rohit'shub <104639942+tngomega@users.noreply.github.com>
2023-10-05 21:35:54 +0530

    Add files via upload

---
+++ day12Operators/12dayhw.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+					/*1*/
+	
+//	int temp;
+//	cout<<"Enter the temperature in Fahrenheit :";
+//	cin>>temp;
+//	
+//	if(temp>=70 && temp<=90)
+//	cout<<"yes";
+//	else
+//	cout<<"no";
+//	
+
+								/*2*/
+								
+//	int n;
+//	cout<<"Enter the no. :";
+//	cin>>n;
+//	
+//	if(n>0)
+//	{
+//		
+//	  if(n%2==0)
+//	  cout<<"yes";
+//	  else
+//	  cout<<"no";	
+//	}							
+
+
+//int age;
+//cout<<"Enter the age :";
+//cin>>age;
+//
+//if(age>=13 && age<=19)
+//cout<<"yes";
+//
+//else
+//cout<<"no";
+
+
+//int a,b,c;
+//
+//cout<<"Enter the numbers :";
+//cin>>a>>b>>c;
+//
+//if(a>b || a>c)
+//cout<<"yes";
+//else
+//cout<<"no";
+
+
+
+
+
+
+
+
+	 
+//	 int z=
+//    2*3-48==5/4*6;
+//
+//	cout<<z;
+
+
+//int z= 6<<2-4*8/2;
+//cout<<z;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+}
