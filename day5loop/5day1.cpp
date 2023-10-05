ba36356370c745b0417443a43f74db69283efa53
Rohit'shub <104639942+tngomega@users.noreply.github.com>
2023-10-05 21:33:26 +0530

    Add files via upload

---
+++ day5loop/5day1.cpp
@@ -0,0 +1,146 @@
+                                         /*For Loop Advance*/
+                                         
+                                         
+#include <iostream>
+using namespace std;
+int main(){
+//	int i;
+//	for(i=101;i<=200;i++)
+//	{
+//		cout<<i<<endl;
+//	}
+
+//
+//
+//     for(char c='a';c<='z';c++)
+//     {
+//     	cout<<c<<" ";
+//	 }
+
+
+//for(int i=10; i>=1;i--)
+//{
+//	cout<<i<<" ";
+//}
+//
+
+
+//int n;
+//cout<<"ENTER THE NUMBER ";
+//cin>>n;
+//cout<<"ENTER THE TABLE OF GIVEN NUMBER :\n";
+//for(int i=1;i<=10;i++)
+//{
+//	cout<<n<<"*"<<i<<"="<<n*i<<endl;
+//}
+//
+
+
+
+//int n,pow;
+//cout<<"ENTER THE NUMBER :";
+//cin>>n; 
+//cout<<"ENTER THE POWER :";
+//cin>>pow; 
+//
+//int num=n;
+//for(int i=1;i<pow;i++)
+//{
+//	num=num*n;
+//}
+//
+//cout<<n<<"power"<<pow<<"="<<num;
+//
+
+/*sum of n natural no.*/
+
+//int n;
+//cout<<"ENTER THE NUMBER :";
+//cin>>n;
+//
+//int sum=0;
+//for(int i=1;i<=n;i++)
+//{
+//	sum+= i;
+//}
+//
+//cout<<"Sum="<<sum<<endl;;
+//
+//cout<<(n*(n+1))/2;
+//
+//
+
+
+
+/*Factorial*/
+
+//    
+//    
+//    int n;
+//    long long fact=1;
+//    
+//    cout<<"ENTER THE NUMBER :\n";
+//    cin>>n;
+//    
+//    for(int i=1;i<=n;i++)
+//    {
+//    	fact*=i;
+//	}
+//	
+//	cout<<"FACTORIAL OF "<<n<<"="<<fact;
+//    
+//
+//
+
+
+
+
+          /*Prime Numbers*/
+
+//
+//int n;
+//cout<<"ENTER THE NUMBER :";
+//cin>>n;
+//
+//if(n<2)
+//{
+//	cout<<"NOT A PRIME NO.";
+//	return 0;
+//}
+//
+//else
+//{
+//	for(int i=2;i<n;i++)
+//	{
+//		if(n%i==0)
+//		{
+//			cout<<"NOT A PRIME NUMBER ";
+//			return 0;
+//		}
+//	 
+//	}
+//	cout<<"PRIME NUMBER";
+//	return 0;
+//}
+//
+
+
+
+         /*FIBONACCI SERIES*/
+         
+         
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+}                                         
