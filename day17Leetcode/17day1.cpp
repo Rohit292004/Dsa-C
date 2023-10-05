db3265f8928a08c1bf6438dfca457bb5c161158d
Rohit'shub <104639942+tngomega@users.noreply.github.com>
2023-10-05 21:37:20 +0530

    Add files via upload

---
+++ day17Leetcode/17day1.cpp
@@ -0,0 +1,155 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	
+		/*1. sum of digits is single*/
+//	int n,rem,sum=0;
+//	
+//	cout<<"Enter the n:";
+//	cin>>n;
+//	
+//while(n>9)
+//{
+//		while(n>0)
+//	{
+//		rem=n%10;
+//		n/=10;
+//		sum+=rem;
+//	}
+//	
+//	cout<<sum;
+//}
+	
+ 
+ 		/*2.  leap year*/
+ 
+ 
+ // 	int isLeap(int n){
+// 
+//     if(n%400==0)
+//     return 1;
+//     else if(n%4==0 && n%100!=0)
+//     return 1;
+//     else
+//      return 0;
+//     }	
+ 
+ 
+ 
+ 
+ 
+			/*3 reverse int*/
+
+
+//int num, rem , ans=0;
+//
+//cout<<"Enter num ";
+//cin>>num;
+//
+//while(num!=0)
+//{
+//	rem=num%10;
+//	num/=10;
+//	ans=ans*10+rem;
+//	
+//	//123
+////	3*10+2   =>32  =>32*10+1   =>321
+//}
+//cout<<ans;
+
+
+					/*power of 2*/
+
+ 
+//    int n;
+//    cout << "Enter n: ";
+//    cin >> n;
+//
+//    if (n <= 0) {
+//        cout << "Invalid input: n must be a positive integer or zero." << endl;
+//        return 0;
+//    }
+//
+//    while (n != 1) {
+//        if (n % 2 == 1)
+//            return 0;
+//
+//        n = n / 2;
+//    }
+//
+//    return 1;
+
+
+
+
+
+								/*sqrt*/
+
+//not worked
+
+
+//								
+//int n;
+//cout<<"Enter number :";
+//cin>>n;
+//
+//for(int i=1;i<=n;i++)
+//{
+//	if(i*i==n)
+//	cout<<i;
+//	else if(i*i>n)
+//	cout<<i;
+//	
+//}
+
+
+
+			/*Palindrome*/
+			
+
+//int num ;
+//cout<<"Enter number :";
+//cin>>num;
+//
+//int n=num,rem,ans=0;
+//
+//for(int i=1;i<=n ;)
+//{
+//	rem=n%10;
+//	n/=10;
+//	//121=>  1*10 +2
+//	// 12      12*10 +2
+//	//
+//	ans=ans*10+rem;
+//}
+//cout<<"ans ="<<ans ;
+//
+//system("\npause");
+//
+//system("cls");
+//
+//cout<<"Check"<<endl;
+//
+//if(num==ans)
+//cout<<"palindrome";
+//else
+//cout<<"Not palindrome";
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+ 
