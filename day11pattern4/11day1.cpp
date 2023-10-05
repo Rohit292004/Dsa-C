e034cd14c9016017a211f93e757a1d3538811587
Rohit'shub <104639942+tngomega@users.noreply.github.com>
2023-10-05 21:35:37 +0530

    Add files via upload

---
+++ day11pattern4/11day1.cpp
@@ -0,0 +1,320 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+//	int n;
+//	cout<<"ENTER N :";
+//	cin>>n;
+//	
+//	for(int r=1;r<=n;r++)
+//	{
+//		for(int c=1;c<=n-r;c++)
+//		{
+//			cout<<" ";
+//		}
+//		
+//		for(int c=1;c<2*r;c++)
+//		{
+//			cout<<"*";
+//		}
+//		
+//		for(int c=1;c<=n-r;c++)
+//		{
+//			cout<<" ";
+//		}
+//		
+//		cout<<endl;
+//	}
+
+														/**/
+
+
+//    1
+//   121
+//  12321
+// 1234321
+//123454321
+//int n;
+//cout<<"enter n :";
+//cin>>n;
+//
+//for(int r=1;r<=n;r++)
+//{
+//	for(int c=1;c<=n-r;c++)
+//	{
+//		cout<<" ";
+//	}
+//      
+//	  
+//	  for(int c=1;c<=r;c++)             //1 to row increase
+//	  {
+//	  	cout<<c;
+//		  }	
+//		  
+//	  for(int c=r-1;c>=1;c--)            //r-1 to 1 decrease
+//	  {
+//	  	cout<<c;
+//			}	  
+//
+//
+//	for(int c=1;c<=n-r;c++)
+//	{
+//		cout<<" ";
+//	}	
+//	
+//	cout<<endl;
+//
+//}
+
+														/**/
+
+
+//   *********
+//    *******
+//     *****
+//      ***
+//       *
+
+//int n;
+//cout<<"Enter n :";
+//cin>>n;
+//
+//for(int r=1;r<=n;r++)
+//{
+//	for(int c=1;c<r;c++)
+//	{
+//		cout<<" ";
+//	}
+//	
+//	
+//	for(int c=r;c<=2*n-r;c++)
+//	{
+//		cout<<"*";
+//	}
+//	
+//	for(int c=1;c<r;c++)
+//	{
+//		cout<<" ";
+//	}
+//	
+//	cout<<endl;
+//}
+
+														/*OR*/
+														
+//int r ,c ,n;
+//cout<<"enter n ";
+//cin>>n;
+//
+//for(r=n;r>=1;r--)
+//{
+//   for(c=1;c<=n-r;c++)
+//   {
+//      cout<<" ";   	
+//   }	
+//   
+//   for(c=1;c<=2*r-1;c++)
+//   {
+//   	cout<<"*";
+//   }
+//   
+//   cout<<endl;
+//}														
+
+
+													/**/
+//********
+//***  ***
+//**    **
+//*      *
+//*      *
+//**    **
+//***  ***
+//********
+	
+//	int n;
+//	cout<<"enter n ";
+//	cin>>n;
+//	
+//	for(int r=1;r<=n/2;r++)
+//	{
+//	   for(int c=r;c<=n/2;c++)
+//	   {
+//	      cout<<"*";   	
+//	   }	
+//	   for(int c=1;c<r;c++)
+//	   {
+//	   	cout<<" ";
+//	   }
+//	   
+//	    for(int c=1;c<r;c++)
+//	   {
+//	   	cout<<" ";
+//	   }
+//	   
+//	    for(int c=r;c<=n/2;c++)
+//	   {
+//	      cout<<"*";   	
+//	   }
+//	   cout<<endl;
+//	   
+//	   
+//	}												
+//
+//
+//	for(int r=1;r<=n/2;r++)
+//	{
+//	   for(int c=r;c>=1;c--)
+//	   {
+//	      cout<<"*";   	
+//	   }	
+//	   for(int c=n/2;c>r;c--)
+//	   {
+//	   	cout<<" ";
+//	   }
+//	   
+//	   for(int c=n/2;c>r;c--)
+//	   {
+//	   	cout<<" ";
+//	   }
+//	   
+//	    for(int c=r;c>=1;c--)
+//	   {
+//	      cout<<"*";   	
+//	   }
+//	   cout<<endl;
+//	   
+//	   
+//	}	
+
+
+
+
+
+
+											/*OR*/
+//	int n,row,col;
+//	cout<<"enter n ";
+//	cin>>n;
+//												
+//for(int row=n;row>=1;row--)
+//{
+//	for(int col=1;col<=row;col++)
+//	{
+//		cout<<"*";
+//	}
+//	
+//	for(int col=1;col<=2*n-2*row;col++)
+//	{
+//		cout<<" ";
+//	}
+//	
+//	for(int col=1;col<=row;col++)
+//	{
+//		cout<<"*";
+//	}
+//	
+//	cout<<endl;
+//}
+//
+//
+//
+//for(row=1;row<=n;row++)
+//{
+//	for(col=1;col<=row;col++)
+//	{
+//		cout<<"*";
+//	}
+//	
+//	for(col=1;col<=2*n-2*row;col++)
+//	{
+//		cout<<" ";
+//	}
+//	
+//	for(col=1;col<=row;col++)
+//	{
+//		cout<<"*";
+//	}
+//	
+//	cout<<endl;
+//}
+
+
+										/**/
+//*        *
+//**      **
+//***    ***
+//****  ****
+//**********
+//****  ****
+//***    ***
+//**      **
+//*        *
+
+//	int n ,r,c;
+//	cout<<"enter n ";
+//	cin>>n;
+//											
+//
+//for(r=1;r<=n;r++)
+//{
+//	for(c=1;c<=r;c++)
+//{
+//	cout<<"*";
+//}
+//
+//for(c=1;c<=2*n-2*r;c++)
+//{
+//	cout<<" ";
+//}
+//
+//	for(c=1;c<=r;c++)
+//{
+//	cout<<"*";
+//}
+//
+//cout<<endl;
+//}
+//
+//
+//
+//for(r=n-1;r>=1;r--)
+//{
+//	for(c=1;c<=r;c++)
+//{
+//	cout<<"*";
+//}
+//
+//for(c=1;c<=2*n-2*r;c++)
+//{
+//	cout<<" ";
+//}
+//
+//	for(c=1;c<=r;c++)
+//{
+//	cout<<"*";
+//}
+//
+//cout<<endl;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
