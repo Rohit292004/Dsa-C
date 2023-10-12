e539af8137ca6367b8f398a44c6e6356361ae9a2
Rohit'shub <104639942+tngomega@users.noreply.github.com>
2023-10-12 22:26:29 +0530

    Add files via upload

---
+++ day25selectionsort.cpp/1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+
+	cout<<"Enter size :\n";
+	cin>>n;
+	int arr[n];
+	cout<<"Enter the elements  :";
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	
+	cout<<"\nElements are:";
+	
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i];
+	}
+	
+	
+	cout<<"\nSorted form =>";
+	
+	for(int i=0;i<n-1;i++)
+	{
+		int index=i;
+		
+		for(int j=i+1;j<n;j++)
+		{
+			if(arr[j]<arr[index])
+			index=j;
+		}
+		
+		swap(arr[index],arr[i]);
+	}
+	
+		for(int i=0;i<n;i++)
+	{
+		cout<<arr[i];
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
